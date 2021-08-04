55b8136359be1df90c15bd287ecfc32af6dc9dcb
sami-p <sami.peterson14@gmail.com>
2021-08-04 11:46:42 -0500

    Display all teams and their attributes on competition show page

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe 'The Competition Show Page' do
   before :each do
     @competition_4 = Competition.create!(name: "The Goodest Boy", location: "Ministry of Magic", sport: "Ice Skating For Dogs")
 
+    @team_1 = @competition_4.teams.create!(nickname: "Anything Is Pawsible", hometown: "Boston")
+    @team_2 = @competition_4.teams.create!(nickname: "The Ultimutt Homies", hometown: "Aspen")
+    @team_3 = @competition_4.teams.create!(nickname: "Pug Life", hometown: "Wheaton")
+    @team_4 = @competition_4.teams.create!(nickname: "Bad To The Bone", hometown: "Dallas")
+    @team_5 = Team.create!(nickname: "Ruff Around The Edges", hometown: "Jackson")
+
     visit "/competitions/#{@competition_4.id}"
   end
 
@@ -18,7 +24,18 @@ RSpec.describe 'The Competition Show Page' do
     expect(page).to have_content(@competition_4.sport)
   end
 
-  xit 'displays the nickname and hometown of all teams in the competition' do
+  it 'displays the nickname and hometown of all teams in the competition' do
+    expect(page).to have_content(@team_1.nickname)
+    expect(page).to have_content(@team_2.nickname)
+    expect(page).to have_content(@team_3.nickname)
+    expect(page).to have_content(@team_4.nickname)
+    expect(page).to_not have_content(@team_5.nickname)
+
+    expect(page).to have_content(@team_1.hometown)
+    expect(page).to have_content(@team_2.hometown)
+    expect(page).to have_content(@team_3.hometown)
+    expect(page).to have_content(@team_4.hometown)
+    expect(page).to_not have_content(@team_5.hometown)
   end
 
   xit 'displays the average age of all players in the competition' do
